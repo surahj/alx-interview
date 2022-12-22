8a5e33fbf50c961f9a87eb07306147c06da1c5d7
surahj <odebodeopeyemi@gmail.com>
2022-12-22 13:02:50 +0000

    Fix error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""
-pascal triangle that returns a list of lists of
-integers representing the Pascal’s triangle of n
-"""
+""" Pascal Triangle """
 
 
 def pascal_triangle(n):
-    if (n <= 0):
-        return []
+    """calculate pascal, return list"""
+    pascal_list = []
 
-    pascal = []
+    if n <= 0:
+        return pascal_list
+
     for i in range(n):
-        pascal.append([])
-        pascal[i].append(1)
-
-        for j in range(1, i):
-            pascal[i].append(pascal[i-1][j-1] + pascal[i-1][j])
-
-        if (i != 0):
-            pascal[i].append(1)
-
-    return pascal
+        temp_list = []
+        for j in range(i + 1):
+            if (j == 0 or j == i):
+                temp_list.append(1)
+            else:
+                temp_list.append(pascal_list[i-1][j-1] + pascal_list[i-1][j])
+        pascal_list.append(temp_list)
+    return pascal_list
