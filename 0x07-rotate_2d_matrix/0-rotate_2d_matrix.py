d7b6ed0661fb98664a5f64d9f0229445c3b98481
surahj <odebodeopeyemi@gmail.com>
2023-02-16 16:35:56 +0100

    Fix error

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,22 +2,11 @@
 """
 Rotate 2D Matrix
 """
-import copy
 
 
 def rotate_2d_matrix(matrix):
+    """ Given an n x n 2D matrix, rotate it 90 degrees clockwise
     """
-    Function that takes n x n 2D matrix, rotate it 90 degrees clockwise
-    Args:
-      matrix: n x n matrix
-    Return:
-      matrix that has been rotated
-    """
-    matrix_copy = copy.deepcopy(matrix)
-    matrix_len = len(matrix)
-
-    for i in range(matrix_len):
-        for j in range(matrix_len):
-            matrix[i][j] = matrix_copy[matrix_len-1-j][i]
-
-    return matrix
+    for index, value in enumerate(zip(*reversed(matrix))):
+        print(index, value)
+        matrix[index] = list(value)
